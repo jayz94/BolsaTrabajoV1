8afe152e6443415db6e3d1cf8a173bba07fd1a67
juan.carlos <jayz.omega@gmail.com>
2017-03-31 23:13:45 -0600

    Primer commit de subida del proyecto

---
+++ BolsaTrabajoV1/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BolsaTrabajoV1
+{
+    public class BundleConfig
+    {
+        // Para obtener más información sobre Bundles, visite http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jsPlantilla").Include(
+                        "~/Scripts/jsPlantilla/jquery.min.js",
+                        "~/Scripts/jsPlantilla/bootstrap.min.js",
+                        "~/Scripts/jsPlantilla/Char.min.js",
+                        "~/Scripts/jsPlantilla/custom.min.js",
+                        "~/Scripts/jsPlantilla/fastclick.js",
+                        "~/Scripts/jsPlantilla/nprogress.js"));
+
+            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            // preparado para la producción y podrá utilizar la herramienta de compilación disponible en http://modernizr.com para seleccionar solo las pruebas que necesite.
+            bundles.Add(new StyleBundle("~/Content/cssPlantilla").Include(
+                      "~/Content/cssPlantilla/bootstrap.min.css",
+                      "~/Content/cssPlantilla/font-awesome.min.css",
+                      "~/Content/cssPlantilla/nprogress.css",
+                      "~/Content/cssPlantilla/green.css",
+                      "~/Content/cssPlantilla/bootstrap-progressbar-3.3.4.min.css",
+                      "~/Content/cssPlantilla/jqvmap.min.css",
+                      "~/Content/cssPlantilla/daterangepicker.css",
+                      "~/Content/cssPlantilla/custom.min.css"));
+
+        }
+    }
+}
---
+++ BolsaTrabajoV1/Controllers/LogroController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BolsaTrabajoV1.Models;
+
+namespace BolsaTrabajoV1.Controllers
+{
+    public class LogroController : Controller
+    {
+        private BolsaTrabajoV1Entities1 db = new BolsaTrabajoV1Entities1();
+
+        // GET: Logro
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Logro.ToListAsync());
+        }
+
+        // GET: Logro/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logro logro = await db.Logro.FindAsync(id);
+            if (logro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logro);
+        }
+
+        // GET: Logro/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logro/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idLogro,tituloLogro,fechaLogro,otorganteLogro,descripcionLogro")] Logro logro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logro.Add(logro);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(logro);
+        }
+
+        // GET: Logro/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logro logro = await db.Logro.FindAsync(id);
+            if (logro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logro);
+        }
+
+        // POST: Logro/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idLogro,tituloLogro,fechaLogro,otorganteLogro,descripcionLogro")] Logro logro)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(logro).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(logro);
+        }
+
+        // GET: Logro/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logro logro = await db.Logro.FindAsync(id);
+            if (logro == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logro);
+        }
+
+        // POST: Logro/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Logro logro = await db.Logro.FindAsync(id);
+            db.Logro.Remove(logro);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
