f34a7bda3269e6b031e003b6af5f302b4bc95878
juan.carlos <jayz.omega@gmail.com>
2017-06-10 21:34:58 -0600

    subiendo tipo de datps By JC

---
+++ BolsaTrabajoV1/Models/PostulanteCurriculum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BolsaTrabajoV1.Models
+{
+    public class PostulanteCurriculum
+    {
+        public String nombre { get; set; }
+        public String genero { get; set; }
+        public int edad { get; set; }
+        public String institucion { get; set; }
+        public String habilidad { get; set; }
+        public String idioma { get; set; }
+        public String titulo { get; set; }
+        public int idpostulante { get; set; }
+        public String municipio { get; set; }
+    }
+}
